e7af90ea273ceebfa0a17598afd5f8be3cfa66eb
Devin Hunter <78935602+Devhunter16@users.noreply.github.com>
2023-02-08 19:18:35 -0700

    Update MealItem.js
    Added form.

--- src/components/Meals/MealItem.js
+++ src/components/Meals/MealItem.js
@@ -1,15 +1,19 @@
 import classes from "./MealItem.module.css";
+import MealItemForm from "./MealItemForm";
 
 const MealItem = (props) => {
   // This ensures that there'll be a dollar sign before each price and decimal will be fixed to two places
   const price = `$${props.mealPrice.toFixed(2)}`;
-  
+
   return (
-    <div className={classes.meal}>
+    <li className={classes.meal}>
       <h3>{props.mealName}</h3>
       <div className={classes.description}>{props.mealDescription}</div>
       <div className={classes.price}>{price}</div>
-    </div>
+      <div>
+        <MealItemForm />
+      </div>
+    </li>
   );
 };
 

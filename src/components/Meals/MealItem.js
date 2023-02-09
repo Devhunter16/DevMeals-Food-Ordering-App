2b25145ddf47a1f82112f684b7032ac9f1f2a880
Devin Hunter <78935602+Devhunter16@users.noreply.github.com>
2023-02-08 19:05:03 -0700

    Update MealItem.js
    Added price variable which ensures a dollar sign before each price and two decimal points after.

--- src/components/Meals/MealItem.js
+++ src/components/Meals/MealItem.js
@@ -1,11 +1,14 @@
 import classes from "./MealItem.module.css";
 
 const MealItem = (props) => {
+  // This ensures that there'll be a dollar sign before each price and decimal will be fixed to two places
+  const price = `$${props.mealPrice.toFixed(2)}`;
+  
   return (
     <div className={classes.meal}>
       <h3>{props.mealName}</h3>
       <div className={classes.description}>{props.mealDescription}</div>
-      <div className={classes.price}>{props.mealPrice}</div>
+      <div className={classes.price}>{price}</div>
     </div>
   );
 };

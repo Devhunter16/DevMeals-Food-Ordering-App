6acbc9e58957d544a2e6a5585a3572948663631b
Devin Hunter <78935602+Devhunter16@users.noreply.github.com>
2023-02-08 18:54:57 -0700

    Update App.js
    Added <Meals /> custom component.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+import "./App.css";
 
 function App() {
   return (
     <>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </>
   );
 }

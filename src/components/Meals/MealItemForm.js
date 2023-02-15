43465859927dc58c50005940f0e7f4f719f798d0
Devin Hunter <78935602+Devhunter16@users.noreply.github.com>
2023-02-15 16:40:17 -0700

    Update MealItemForm.js
    Added custom Input component.

--- src/components/Meals/MealItemForm.js
+++ src/components/Meals/MealItemForm.js
@@ -1,9 +1,22 @@
 import classes from "./MealItemForm.module.css";
+import Input from "../UI/Input";
 
+// Passing in an object named input with a bunch of key-value value pairs that we
+// will pass to our Input
 const MealItemForm = () => {
   return (
     <form className={classes.form}>
-      <input />
+      <Input
+        label="Amount"
+        input={{
+          id: "amount",
+          type: "number",
+          min: "1",
+          max: "5",
+          step: "1",
+          defaultValue: "1",
+        }}
+      />
       <button>+ Add</button>
     </form>
   );

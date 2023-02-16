090a1320b82ee9a2731ec2ffd21da5ac0408b9f3
Devin Hunter <78935602+Devhunter16@users.noreply.github.com>
2023-02-15 18:05:10 -0700

    Add files via upload
    Adding Cart custom component and styles.

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,30 @@
+import classes from "./Cart.module.css";
+
+// Mapping each cart item to an <li> element within a <ul> element that
+// displays the item's name
+// The cart-items className is written like that because of the dash
+const Cart = (props) => {
+  const cartItems = (
+    <ul className={classes["cart-items"]}>
+      {[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map((item) => (
+        <li>{item.name}</li>
+      ))}
+    </ul>
+  );
+
+  return (
+    <>
+      <div>{cartitems}</div>
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span>35.62</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes["button--alt"]}>Close</button>
+        <button className={classes.button}>Order</button>
+      </div>
+    </>
+  );
+};
+
+export default Cart;

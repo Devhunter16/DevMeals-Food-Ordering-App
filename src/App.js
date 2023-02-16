9483514ededa361b9c03539c0355211e418a6441
Devin Hunter <78935602+Devhunter16@users.noreply.github.com>
2023-02-15 18:44:13 -0700

    Update App.js
    Added Cart custom component.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
 import "./App.css";
 
 function App() {
   return (
     <>
       <Header />
+      <Cart />
       <main>
         <Meals />
       </main>

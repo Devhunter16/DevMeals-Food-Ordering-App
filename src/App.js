e2ca37adb7ba05b9d02c8c4f479ec992346c03c0
Devin Hunter <78935602+Devhunter16@users.noreply.github.com>
2023-02-19 17:41:25 -0700

    Update App.js
    Added some useState hook functionality in order to open and close the cart.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
+import { useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
 import "./App.css";
 
 function App() {
+  const [cartIsShown, setCartVisibility] = useState(false);
+
+  const setCartVisible = () => {
+    setCartVisibility(true);
+  };
+
+  const setCartInvisible = () => {
+    setCartVisibility(false);
+  };
+
   return (
     <>
-      <Header />
-      <Cart />
+      {cartIsShown && <Cart onClose={setCartInvisible}/>}
+      <Header onShowCart={setCartVisible}/>
       <main>
         <Meals />
       </main>

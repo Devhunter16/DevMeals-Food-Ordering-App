2d546f569d78e6fd06d4e7a522fa972e0896fb54
Devin Hunter <78935602+Devhunter16@users.noreply.github.com>
2023-02-21 10:08:37 -0700

    Update HeaderCartButton.js
    Added useContext hook and code that will update the number of items in the cart.

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,33 @@
+import { useContext } from "react";
 import classes from "./HeaderCartButton.module.css";
+import CartContext from "../../store/cart-context";
 import CartIcon from "../Cart/CartIcon";
 
 const HeaderCartButton = (props) => {
+  // Using useContext hook here to essentially subscribe to the 
+  // values within CartContext so that any time any of those values
+  // change, the subscription we have made to useContext auto-updates
+  // those values and our component re-renders to reflect the new
+  // values in the UI.
+  const cartCtx = useContext(CartContext);
+
+  // .reduce() is a method that allows us to transform an array of
+  // data into a signle calue. A number that represents the number
+  // of items in the cart in this context. The method wants two
+  // parameters, a value (currNumber) that holds its value and is 
+  // updated every execution, the "0" value in .reduce() is the intial
+  // starting value. 
+  const numberOfCartItems = cartCtx.items.reduce((currentNumber, item) => {
+    return currentNumber + item.amount;
+  }, 0);
+
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 };

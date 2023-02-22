382efb0318c843421ca21592acc05aca88c322d9
Devin Hunter <78935602+Devhunter16@users.noreply.github.com>
2023-02-21 18:21:46 -0700

    Update MealItem.js

--- src/components/Meals/MealItem.js
+++ src/components/Meals/MealItem.js
@@ -1,9 +1,22 @@
+import { useContext } from "react";
 import classes from "./MealItem.module.css";
 import MealItemForm from "./MealItemForm";
+import CartContext from "../../store/cart-context";
 
 const MealItem = (props) => {
+  const cartCtx = useContext(CartContext);
+
   // This ensures that there'll be a dollar sign before each price and decimal will be fixed to two places
   const price = `$${props.mealPrice.toFixed(2)}`;
+
+  const addToCartHandler = (amount) => {
+    cartCtx.addItem({
+      id: props.id,
+      name: props.name,
+      amount: amount,
+      price: props.price,
+    });
+  };
 
   return (
     <li className={classes.meal}>
@@ -11,7 +24,7 @@ const MealItem = (props) => {
       <div className={classes.description}>{props.mealDescription}</div>
       <div className={classes.price}>{price}</div>
       <div>
-        <MealItemForm id={props.id} />
+        <MealItemForm onAddToCart={addToCartHandler} id={props.id} />
       </div>
     </li>
   );

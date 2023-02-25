f4066b1b681c1016aa78e45dd2cacfbb6174bc7d
Devin Hunter <78935602+Devhunter16@users.noreply.github.com>
2023-02-25 15:32:53 -0700

    Update Cart.js
    Added logic to the cart to add up all items and display them nicely.

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,14 +1,51 @@
-import classes from "./Cart.module.css";
-import Modal from "../UI/Modal";
+import { useContext } from 'react';
 
-// Mapping each cart item to an <li> element within a <ul> element that
-// displays the item's name
-// The cart-items className is written like that because of the dash
+import classes from "./Cart.module.css";
+import CartItem from "./CartItem";
+import Modal from "../UI/Modal";
+import CartContext from "../../store/cart-context";
+
 const Cart = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0;
+
+  const cartItemRemoveHandler = (id) => {
+
+  }
+
+  const cartItemAddHandler = (item) => {
+
+  }
+
   const cartItems = (
+    // Mapping each cart item to a custom CartItem element within a <ul> element 
+    // to make an unordered list of CartItems
+    // The cart-items className is written like that because of the dash
     <ul className={classes["cart-items"]}>
-      {[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map((item) => (
-        <li>{item.name}</li>
+      {cartCtx.items.map((item) => (
+        // Each CartItem must have an id labeled "key" because this helps
+        // React identify which items have been added/removed/re-ordered
+        // within a list
+        <CartItem 
+        key={item.id} 
+        name={item.name} 
+        amount={item.amount} 
+        price={item.price} 
+        // Explanation of .bind() here: If we want to pass additional 
+        // params (not only the event which is passed by default), 
+        // we can't just write a simple reference like onClick={clickHandler}
+        // And we can't write onClick={clickHandler(param)} since this 
+        // would call the function immediately (and not only when the 
+        // cart item is clicked). if we want to pass params, we can 
+        // either use bind (the first param is not used here, so we 
+        // can write anything in this place) onClick={clickHandler.bind(null, param)}
+        // or we can create an anonymous function: onClick={() => clickHandler(param)}
+        // Both options are equivalent.
+        onRemove={cartItemRemoveHandler.bind(null, item.id)} 
+        onAdd={cartItemAddHandler.bind(null, item)}
+        />
       ))}
     </ul>
   );
@@ -18,11 +55,11 @@ const Cart = (props) => {
       <div>{cartItems}</div>
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>35.62</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
         <button className={classes["button--alt"]} onClick={props.onClose}>Close</button>
-        <button className={classes.button}>Order</button>
+        {hasItems && <button className={classes.button}>Order</button>}
       </div>
     </Modal>
   );

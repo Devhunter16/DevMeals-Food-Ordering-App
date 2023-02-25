3ea21670cf213ded166a779ddc39ce2b9df6a15b
Devin Hunter <78935602+Devhunter16@users.noreply.github.com>
2023-02-25 15:32:08 -0700

    Update MealItem.js
    Had to fix the values of the name and price keys in the addToCartHandler function to fix a bug in the code that was presenting NaN where the price is supposed to be in the cart.

--- src/components/Meals/MealItem.js
+++ src/components/Meals/MealItem.js
@@ -12,9 +12,9 @@ const MealItem = (props) => {
   const addToCartHandler = (amount) => {
     cartCtx.addItem({
       id: props.id,
-      name: props.name,
+      name: props.mealName,
       amount: amount,
-      price: props.price,
+      price: props.mealPrice,
     });
   };
 
